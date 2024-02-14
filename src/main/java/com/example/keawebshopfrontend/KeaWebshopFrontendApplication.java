7407ca3aa08f2a624cf4fd6ca3d500220f546a05
Nduris <nduris@hotmail.com>
2024-02-14 10:44:27 +0100

    Initial Commit

---
+++ src/main/java/com/example/keawebshopfrontend/KeaWebshopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.keawebshopfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeaWebshopFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeaWebshopFrontendApplication.class, args);
+    }
+
+}
